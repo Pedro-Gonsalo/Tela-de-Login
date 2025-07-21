bb11ba74bfcd603ebf803d25ff799c189b41a435
PedroGoncaloGRV <pedro.henrique@grvsoftware.com.br>
2025-07-21 17:38:18 -0300

    feat: terminado a parte de criação do banco no start do serviço

--- back_end/uMssqlDriver.pas
+++ back_end/uMssqlDriver.pas
@@ -4,92 +4,117 @@ interface
 
 uses
   System.SysUtils,
-  FireDAC.Comp.Client,
+  FireDAC.Comp.Client, FireDAC.DApt,
   FireDAC.Stan.Intf, FireDAC.Phys.Intf,
   FireDAC.Phys.MSSQL, FireDAC.Phys.MSSQLDef,
   FireDAC.Stan.Option, FireDAC.Stan.Pool, FireDAC.Stan.Async,
   uIniConfigStore,
-  uCreateDatabase;      // IDatabaseDriver
+  uCreateDatabase; // IDatabaseDriver
+
+const
+  DEFAULT_DATABASE = 'master';
 
 type
   TMssqlDriver = class(TInterfacedObject, IDatabaseDriver)
   private
-    procedure ConfigureConnection(const Conn: TFDConnection;
-      const P: TConnectionParams);
+    FConnection: TFDConnection;
+    procedure ConfigureConnection(const Params: TConnectionParams);
+    function InternalCheckDatabaseExists(const DatabaseName: string): Boolean;
   public
-    function  DatabaseExists(const P: TConnectionParams): Boolean;
-    procedure CreateDatabaseIfNotExists(const P: TConnectionParams);
+    constructor Create;
+    destructor Destroy; override;
+    function DatabaseExists(const Params: TConnectionParams): Boolean;
+    procedure CreateDatabaseIfNotExists(const Params: TConnectionParams);
   end;
 
 implementation
 
-procedure TMssqlDriver.ConfigureConnection(const Conn: TFDConnection;
-  const P: TConnectionParams);
+{ TMssqlDriver }
+
+constructor TMssqlDriver.Create;
+begin
+  inherited;
+  FConnection := TFDConnection.Create(nil);
+end;
+
+destructor TMssqlDriver.Destroy;
+begin
+  if Assigned(FConnection) then
+  begin
+    if FConnection.Connected then
+      FConnection.Close;
+    FreeAndNil(FConnection);
+  end;
+  inherited;
+end;
+
+procedure TMssqlDriver.ConfigureConnection(const Params: TConnectionParams);
 begin
   try
-    Conn.Params.Clear;
-    with Conn.Params do
+    FConnection.Params.Clear;
+    with FConnection.Params do
     begin
       Add('DriverID=MSSQL');
-      if P.Port.IsEmpty
-        then Add('Server=' + P.Host)
-        else Add('Server=' + P.Host + ',' + P.Port);
-      Add('Database=' + P.DatabaseName);
-      Add('User_Name=' + P.User);
-      Add('Password=' + P.Password);
+      if Params.Port.IsEmpty then
+        Add('Server=' + Params.Host)
+      else
+        Add('Server=' + Params.Host + ',' + Params.Port);
+      Add('Database=' + DEFAULT_DATABASE); // Sempre conecta ao master
+      Add('User_Name=' + Params.User);
+      Add('Password=' + Params.Password);
     end;
-    Conn.LoginPrompt := False;
+    FConnection.LoginPrompt := False;
+    FConnection.Open; // Conecta ao master
   except
     on E: Exception do
       raise Exception.Create('ConfigureConnection: ' + E.Message);
   end;
 end;
 
-function TMssqlDriver.DatabaseExists(const P: TConnectionParams): Boolean;
+function TMssqlDriver.InternalCheckDatabaseExists(const DatabaseName: string): Boolean;
 var
-  Conn: TFDConnection;
+  Query: TFDQuery;
 begin
-  Result := False;
-  Conn := TFDConnection.Create(nil);
+  Query := TFDQuery.Create(nil);
   try
-    try
-      ConfigureConnection(Conn, P);
-      try
-        Conn.Open;
-        Result := True;
-      except
-        Result := False;
-      end;
-    except
-      on E: Exception do
-        raise Exception.Create('DatabaseExists: ' + E.Message);
-    end;
+    Query.Connection := FConnection;
+    Query.SQL.Text := 'SELECT 1 FROM sys.databases WHERE name = :DatabaseName';
+    Query.ParamByName('DatabaseName').AsString := DatabaseName;
+    Query.Open;
+    Result := not Query.IsEmpty;
   finally
-    Conn.Free;
+    Query.Free;
   end;
 end;
 
-procedure TMssqlDriver.CreateDatabaseIfNotExists(const P: TConnectionParams);
-var
-  Conn: TFDConnection;
-  Q   : TFDQuery;
+function TMssqlDriver.DatabaseExists(const Params: TConnectionParams): Boolean;
 begin
   try
-    if DatabaseExists(P) then
-      Exit;
+    ConfigureConnection(Params); // Conecta ao master
+    Result := InternalCheckDatabaseExists(Params.DatabaseName); // Verifica se o banco alvo existe
+  except
+    on E: Exception do
+      raise Exception.Create('DatabaseExists: ' + E.Message);
+  end;
+end;
 
-    Conn := TFDConnection.Create(nil);
-    Q    := TFDQuery.Create(nil);
-    try
-      ConfigureConnection(Conn, P);
-      Conn.Open;
+procedure TMssqlDriver.CreateDatabaseIfNotExists(const Params: TConnectionParams);
+var
+  Query: TFDQuery;
+begin
+  try
+    ConfigureConnection(Params); // Conecta ao master
 
-      Q.Connection := Conn;
-      Q.SQL.Text := Format('CREATE DATABASE [%s]', [P.DatabaseName]);
-      Q.ExecSQL;
-    finally
-      Q.Free;
-      Conn.Free;
+    if not InternalCheckDatabaseExists(Params.DatabaseName) then
+    begin
+      Query := TFDQuery.Create(nil);
+      try
+        Query.Connection := FConnection;
+        Query.SQL.Text := Format('CREATE DATABASE [%s]', [Params.DatabaseName]);
+        Query.ExecSQL;
+      finally
+        Query.Free;
+      end;
     end;
   except
     on E: Exception do
@@ -98,4 +123,3 @@ begin
 end;
 
 end.
-
